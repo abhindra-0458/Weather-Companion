43a1ebebaa594a1f2d5ec52a07b04bd9aed5dafe
“[abh!ndra]” <[abhindrakrishnakm@gmail.com]>
2024-08-22 11:00:14 +0530

    port from enivironment variable

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ dotenv.config();
 // console.log(process.env);
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const APIkey = process.env.API_KEY;
